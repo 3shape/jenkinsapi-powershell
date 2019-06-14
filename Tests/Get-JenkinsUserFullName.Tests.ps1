5e266c3dad52f713fcdd99a8f18727691a465608
Tuk <leekhangyeen@gmail.com>
2019-06-14 10:02:24 +0200

    Converting json content to ps custom object to lookup fullname. (#5) (#7)
    * Converting json content to ps custom object to lookup fullname. (#5)

--- Tests/Get-JenkinsUserFullName.Tests.ps1
+++ Tests/Get-JenkinsUserFullName.Tests.ps1
@@ -1,4 +1,4 @@
-Import-Module -Name (Get-ChildItem $PSScriptRoot\..\*.psm1 | Select-Object -first 1).FullName -Force
+Import-Module -Name (Get-ChildItem $PSScriptRoot\..\*.psm1 | Select-Object -First 1).FullName -Force
 . "$PSScriptRoot\..\Public\Get-JenkinsUserFullName.ps1"
 . "$PSScriptRoot\..\Private\Get-JenkinsUserInfo.ps1"
 
@@ -51,14 +51,14 @@ Describe 'Get-JenkinsUserFullName method' {
 
         It 'Retrieves the fullname of the user' {
             Mock -CommandName Get-JenkinsUserInfo { return $UserInfo1 } -Verifiable
-            $result = Get-JenkinsUserFullName -UserName $user -Password $pass
+            $result = Get-JenkinsUserFullName -Username $user1 -Password $pass
             $result | Should -BeExactly $Fullname1
             Assert-MockCalled -CommandName Get-JenkinsUserInfo -Exactly 1
         }
 
         It 'It is looked up, cached and re-used' {
             Mock -CommandName Get-JenkinsUserInfo { return $UserInfo2 } -Verifiable
-            $result = Get-JenkinsUserFullName -UserName $user2 -Password $pass
+            $result = Get-JenkinsUserFullName -Username $user2 -Password $pass
             $result | Should -BeExactly $Fullname2
             Assert-MockCalled -CommandName Get-JenkinsUserInfo -Exactly 2
         }
@@ -68,18 +68,18 @@ Describe 'Get-JenkinsUserFullName method' {
         1st time from lookup, 2nd time from cache' {
 
         It '1st user gets own fullname returned' {
-            Mock -CommandName Invoke-JenkinsRequest { return @{Content = $UserInfo1 } } -Verifiable
-            $result1 = Get-JenkinsUserFullName -UserName $user1 -Password $pass
-            $result2 = Get-JenkinsUserFullName -UserName $user1 -Password $pass -UsernameToLookup $user1
+            Mock -CommandName Invoke-JenkinsRequest { return @{ Content = $UserInfo1 | ConvertTo-Json } } -Verifiable
+            $result1 = Get-JenkinsUserFullName -Username $user1 -Password $pass
+            $result2 = Get-JenkinsUserFullName -Username $user1 -Password $pass -UsernameToLookup $user1
             $result1 | Should -BeExactly $result2
             $result2 | Should -BeExactly $Fullname1
             Assert-MockCalled -CommandName Invoke-JenkinsRequest -Exactly 1
         }
 
         It '2nd user gets own fullname returned' {
-            Mock -CommandName Invoke-JenkinsRequest { return @{Content = $UserInfo2 } } -Verifiable
-            $result1 = Get-JenkinsUserFullName -UserName $user2 -Password $pass
-            $result2 = Get-JenkinsUserFullName -UserName $user2 -Password $pass -UsernameToLookup $user2
+            Mock -CommandName Invoke-JenkinsRequest { return @{ Content = $UserInfo2 | ConvertTo-Json } } -Verifiable
+            $result1 = Get-JenkinsUserFullName -Username $user2 -Password $pass
+            $result2 = Get-JenkinsUserFullName -Username $user2 -Password $pass -UsernameToLookup $user2
             $result1 | Should -BeExactly $result2
             $result2 | Should -BeExactly $Fullname2
             Assert-MockCalled -CommandName Invoke-JenkinsRequest -Exactly 2
@@ -90,15 +90,15 @@ Describe 'Get-JenkinsUserFullName method' {
         1st time from lookup, 2nd time from cache' {
 
         It '1st user looks up fullname of 2nd user' {
-            Mock -CommandName Invoke-JenkinsRequest { return @{Content = $UserInfo2 } } -Verifiable
-            $result2 = Get-JenkinsUserFullName -UserName $user1 -Password $pass -UsernameToLookup $user2
+            Mock -CommandName Invoke-JenkinsRequest { return @{Content = $UserInfo2 | ConvertTo-Json } } -Verifiable
+            $result2 = Get-JenkinsUserFullName -Username $user1 -Password $pass -UsernameToLookup $user2
             $result2 | Should -BeExactly $Fullname2
             Assert-MockCalled -CommandName Invoke-JenkinsRequest -Exactly 1
         }
 
         It '2nd user looks up fullname of 1st user' {
-            Mock -CommandName Invoke-JenkinsRequest { return @{Content = $UserInfo1 } } -Verifiable
-            $result1 = Get-JenkinsUserFullName -UserName $user2 -Password $pass -UsernameToLookup $user2
+            Mock -CommandName Invoke-JenkinsRequest { return @{Content = $UserInfo1 | ConvertTo-Json } } -Verifiable
+            $result1 = Get-JenkinsUserFullName -Username $user2 -Password $pass -UsernameToLookup $user2
             $result1 | Should -BeExactly $Fullname1
             Assert-MockCalled -CommandName Invoke-JenkinsRequest -Exactly 2
         }
